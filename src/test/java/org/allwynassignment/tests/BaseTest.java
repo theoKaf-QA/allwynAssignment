72d2faefbf873400dc432dfbdb90e1275b560bdd
Theo K <link_q-ropol@hotmail.com>
2025-11-15 18:35:58 +0200

    First successful tests

--- src/test/java/org/allwynassignment/tests/BaseTest.java
+++ src/test/java/org/allwynassignment/tests/BaseTest.java
@@ -1,4 +1,49 @@
 package org.allwynassignment.tests;
 
-public class BaseTest {
+import io.qameta.allure.Allure;
+import lombok.extern.slf4j.Slf4j;
+import org.allwynassignment.APITestAutomation.operations.actions.AuthorActions;
+import org.allwynassignment.APITestAutomation.operations.actions.BookActions;
+import org.allwynassignment.APITestAutomation.operations.validations.AuthorValidations;
+import org.allwynassignment.APITestAutomation.operations.validations.BookValidations;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+@Slf4j
+public abstract class BaseTest {
+    protected BookActions bookActions;
+    protected AuthorActions authorActions;
+    protected BookValidations bookValidations;
+    protected AuthorValidations authorValidations;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        bookActions = new BookActions();
+        authorActions = new AuthorActions();
+        bookValidations = new BookValidations();
+        authorValidations = new AuthorValidations();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            Allure.addAttachment("Failure Reason", result.getThrowable().getMessage());
+        }
+    }
+
+    protected void assertTrue(boolean condition, String message) {
+        if (!condition) {
+            log.error("Expected condition: {} to be true but it was false. Message:", message);
+            throw new AssertionError(message);
+        }
+    }
+    protected void assertFalse(boolean condition, String message) {
+        if (condition) {
+            log.error("Expected condition: {} to be false but it was true. Message:", message);
+            throw new AssertionError(message);
+        }
+    }
+
 }
--- src/test/java/org/allwynassignment/tests/BooksTests.java
+++ src/test/java/org/allwynassignment/tests/BooksTests.java
@@ -1,4 +1,69 @@
 package org.allwynassignment.tests;
 
-public class BooksTests {
+
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
+import io.qameta.allure.testng.Tags;
+import io.restassured.response.Response;
+import org.allwynassignment.APITestAutomation.models.Book;
+import org.allwynassignment.dataProviders.BooksDataProvider;
+import org.testng.annotations.Test;
+
+@Epic("Allwyn Assignment Test Automation")
+@Owner("Theodoros Kafazis")
+@Feature("Books FakeRestAPI Tests")
+public class BooksTests extends BaseTest{
+
+    @Test(priority = 1, description = "Verify GET books returns 200 and list of books",
+          groups = {"smoke", "regression"})
+    @Tags({@Tag("smoke"), @Tag("regression")})
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that GET /Books endpoint returns successful response with a list of books")
+    public void testGetAllBooks_Success() {
+        Response response = bookActions.getAllBooks();
+        bookValidations.verifyStatusCode(response, 200);
+
+        Book[] books = bookValidations.parseListResponse(response);
+        bookValidations.verifyListNotEmpty(books);
+
+        Book firstBook = books[0];
+        bookValidations.verifyHasValidID(firstBook);
+        bookValidations.verifyBookHasValidTitle(firstBook);
+    }
+
+
+    @Test(priority = 2, description = "Verify GET book by valid ID returns 200 and book details",
+            groups = {"smoke"})
+    @Tags({@Tag("smoke")})
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that GET /Books/{id} endpoint finds the book with the specified ID and validates its details")
+    public void testGetBookById_ValidId_Success() {
+        int validBookId = 1;
+        Response response = bookActions.getBookById(validBookId);
+
+        bookValidations.verifyStatusCode(response, 200);
+
+        Book book = bookValidations.parseResponse(response);
+        bookValidations.verifyNotNull(book);
+        bookValidations.verifyBookId(book, validBookId);
+        bookValidations.verifyBookHasValidTitle(book);
+    }
+
+
+    @Test(priority = 3, description = "Verify GET book with multiple valid IDs, they all return 200 and book details",
+            groups = {"smoke"},
+            dataProvider = "validBookIds", dataProviderClass = BooksDataProvider.class)
+    @Tags({@Tag("smoke")})
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that GET /Books/{id} endpoint finds the book with the specified ID and validates its details")
+    public void testGetBookById_ValidId_Success(int validBookId) {
+        Response response = bookActions.getBookById(validBookId);
+
+        bookValidations.verifyStatusCode(response, 200);
+
+        Book book = bookValidations.parseResponse(response);
+        bookValidations.verifyNotNull(book);
+        bookValidations.verifyBookId(book, validBookId);
+        bookValidations.verifyBookHasValidTitle(book);
+    }
 }
